5cd6137655bc7af3284430eb7f532404ea69c7da
Marc Hauschildt <marc.hauschildt@gmail.com>
2020-10-05 12:17:49 -0500

    Added Abstract Animal Class

---
+++ src/main/java/edu/kcc/animal/Animal.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.kcc.animal;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author k0519415
+ */
+public abstract class Animal{
+   
+    private String name;
+    private String species;
+    private String gender;
+    private int age;
+    private boolean fixed;
+    private int legs;
+    private BigDecimal weight;
+    private LocalDate dateAdded;
+    private LocalDateTime lastFeedingTime;
+
+    // TODO: Need full constructor
+    
+    // TODO: Need default constructor
+
+    // TODO: Need getName method - Fadwa
+    
+    // TODO: Need setName method - Chase
+    
+    // TODO: Need getSpecies method - Becky
+    
+    // TODO: Need getGender method - Whitney
+    
+    // TODO: Need getAge method - William
+    
+    // TODO: Need setAge method - Joseph
+    
+    // TODO: Need getFixed method - Vinayak
+    
+    // TODO: Need setFixed method - Chantal
+    
+    // TODO: Need getLegs method - Haley
+    
+    // TODO: Need setLegs method - Bonny
+    
+    // TODO: Need getWeight method - Melissa
+    
+    // TODO: Need setWeight method - Ryan
+    
+    // TODO: Need getDateAdded method - Nick
+    
+    // TODO: Need setDateAdded method - Jory
+    
+    // TODO: Need getLastFeedingTime method - Andrea
+    
+    // TODO: Need setLastFeedingTime method
+    
+    // TODO: Need toString method
+    
+    // TODO: Need compareTo method
+}
