6f4afad1cddd08e2ce83584ad25fab3ba47dcb47
Haley Hepker <hailzrene@gmail.com>
2020-10-14 12:58:08 -0500

    add setSpecies method

--- src/main/java/edu/kcc/animal/Animal.java
+++ src/main/java/edu/kcc/animal/Animal.java
@@ -111,9 +111,8 @@ public class Animal {
     // TODO: Need dateValidator method - only allow dates up to a week in the past (Ryan)
     
     // TODO: Need getLastFeedingTime method (Haley)
-        public LocalDateTime getLastFeedingTime(){
-        return lastFeedingTime;
-    }
+
+
     
     public void setLastFeedingTime(LocalDateTime ldt) {
         lastFeedingTime = ldt;
