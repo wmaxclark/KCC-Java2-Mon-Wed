7b10c8190342995c525c4eca1c9c7c5677d8d0a3
Chantal Shirley <Chantal.C.Shirley@gmail.com>
2020-10-28 13:49:41 -0500

    Added AnimalDAOCSV data shell Java class

---
+++ src/main/java/edu/kcc/animal/data/AnimalDAOCSV.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.kcc.animal.data;
+
+/**
+ *
+ * @author Chantal Shirley
+ */
+public class AnimalDAOCSV {
+    // TODO readFromFile()
+    
+    // TODO saveToFile()
+    
+    // TODO verifyAnimalList()
+    
+    // TODO createAnimal(Animal animal)
+    
+    // TODO getAnimalByID(String id)
+    
+    // TODO getAllAnimals()
+    
+    // TODO updateAnimal (Animal original, Animal updated)
+    
+    // TODO deleteAnimal(Animal animal)
+    
+    // TODO deleteAnimal(String id)
+    
+}
