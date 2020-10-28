17b61972daef6cd35a6abe81dfb8f4a46b5d8849
Haley Hepker <hailzrene@gmail.com>
2020-10-28 13:55:51 -0500

    created AnimalDataException

--- src/main/java/edu/kcc/animal/data/AnimalDataException.java
+++ src/main/java/edu/kcc/animal/data/AnimalDataException.java
@@ -3,13 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.kcc.animal;
-
+package edu.kcc.animal.data;
 /**
  *
- * @author Haley
+ * @author Haley Hepker
  */
-
 public class AnimalDataException extends Exception{
     
     public AnimalDataException() {
